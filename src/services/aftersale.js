ec417761f52f920228f9ba9a546594b701c1f43e
jiechud <qiaojienet@163.com>
2020-03-26 19:01:35 +0800

    售后 bug fixed

--- src/services/aftersale.js
+++ src/services/aftersale.js
@@ -7,7 +7,7 @@ import Api from '../config/api';
  *  售后列表
  */
 export async function getAftersaleListApi(payload) {
-  return request.post(Api.AftersaleList, payload);
+  return request.get(Api.AftersaleList, payload);
 }
 
 
